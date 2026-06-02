768ce18c3f40e49b71561af85b55f20802ef2b4b
kelvin-mutugi <kelvinmutugi925@gmail.com>
2026-06-02 21:46:23 +0300

    drawing

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ text_font = pygame.font.Font(None, 50)
 
 sky_surface = pygame.image.load('graphics/backgroung.png').convert()
 ground_surface = pygame.image.load('graphics/road.png').convert()
-text_surface = text_font.render('My Game', False, 'Green')
+
+score_surface = text_font.render('My Game', False, 'Green')
+score_rect = score_surface.get_rect(center = (400, 50))
 
 gost_surface = pygame.image.load('graphics/gost.png').convert_alpha()
 #gost_x_position = 734
@@ -31,13 +33,17 @@ while True:
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (345,50))
+    pygame.draw.rect(screen, 'Red', score_rect)
+    pygame.draw.rect(screen, 'Red', score_rect, 10)
+    screen.blit(score_surface, score_rect)
+    #pygame.draw.line(screen, 'red', (0,0), (800,400), width=1)
+    #pygame.draw.ellipse(screen, 'Red', pygame.Rect(50,50, 100,100))
 
     gost_rectangle.left -= 4
     if gost_rectangle.right < 0 : gost_rectangle.left = 800
 
-    if player_rectangle.colliderect(gost_rectangle):
-        print('')
+    #if player_rectangle.colliderect(gost_rectangle):
+     #   print('')
     
 
     screen.blit(gost_surface, gost_rectangle)
