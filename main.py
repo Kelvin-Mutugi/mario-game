091c02e925e0f12c83f5525be8bc67d44af03289
kelvin-mutugi <kelvinmutugi925@gmail.com>
2026-06-06 15:53:31 +0300

    score feature implementend. The player gets a socore depending on how long they stay alive in the game in seconds

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pygame
 from sys import exit
 
-pygame.init()
+def display_score():
+     current_time = int(pygame.time.get_ticks()/1000 - start_time)
+     score_surf = text_font.render(f'Score: {current_time}', False, 'Green')
+     score_rect = score_surf.get_rect(center = (400, 50))
+     screen.blit(score_surf,score_rect)
 
+
+pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
@@ -11,16 +17,14 @@ text_font = pygame.font.Font(None, 50)
 sky_surface = pygame.image.load('graphics/backgroung.png').convert()
 ground_surface = pygame.image.load('graphics/road.png').convert()
 
-score_surface = text_font.render('My Game', False, 'Green')
-score_rect = score_surface.get_rect(center = (400, 50))
+start_time = 0
 
 Game_over_surface = text_font.render('GAME OVER', False, 'Green')
 Game_over_rect = Game_over_surface.get_rect(center = (400, 200))
 
 gost_surface = pygame.image.load('graphics/gost.png').convert_alpha()
 gost_surface2 = pygame.image.load('graphics/gost.png').convert_alpha()
-#gost_x_position = 734
-#gost_y_position = 295
+
 gost_rectangle = gost_surface.get_rect(topleft = (734,290))
 gost_rectangle2 = gost_surface2.get_rect(top = 150)
 
@@ -48,34 +52,28 @@ while True:
         else:
             if event.type == pygame.MOUSEBUTTONDOWN:
                     game_active = True
-                    gost_rectangle.left = 800         
-                    gost_rectangle2.left = 900
+                    gost_rectangle.left = 800
+                    start_time = pygame.time.get_ticks()/1000
             if event.type == pygame.KEYDOWN and pygame.K_SPACE:
                     game_active = True
                     gost_rectangle.left = 800
-                    gost_rectangle2.left = 900
-
-                
+                    start_time = pygame.time.get_ticks()/1000            
 
     if game_active:
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0,300))
-        pygame.draw.rect(screen, 'Red', score_rect)
-        pygame.draw.rect(screen, 'Red', score_rect, 10)
-        screen.blit(score_surface, score_rect)
+        display_score()
+
         #pygame.draw.line(screen, 'red', (0,0), (800,400), width=1)
         #pygame.draw.ellipse(screen, 'Red', pygame.Rect(50,50, 100,100))
 
         #ENEMYS
-        gost_rectangle.left -= 5
+        gost_rectangle.left -= 6
         if gost_rectangle.right < 0 : gost_rectangle.left = 800
         #if player_rectangle.colliderect(gost_rectangle):
         #   print('')   
         screen.blit(gost_surface, gost_rectangle)
         
-        #gost_rectangle2.left -= 5
-        #if gost_rectangle2.right < -20 : gost_rectangle2.left = 800
-        screen.blit(gost_surface2, gost_rectangle2)
 
         #PLAYER
         player_gravity += 1
@@ -86,8 +84,6 @@ while True:
         #COLLISION
         if gost_rectangle.colliderect(player_rectangle):
             game_active = False
-        if gost_rectangle2.colliderect(player_rectangle):
-             game_active = False
 
     else:
         screen.blit(sky_surface, (0,0))
